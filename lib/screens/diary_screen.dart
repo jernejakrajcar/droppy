bb3a826b2533184691cb538ce94897259bae9c19
jernejak12 <jerneja.krajcar@gmail.com>
2024-01-05 18:20:59 +0100

    popravljen diary screen plus dodani assets

--- lib/screens/diary_screen.dart
+++ lib/screens/diary_screen.dart
@@ -6,27 +6,50 @@ class DiaryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('My Diary'),
-          centerTitle: true,
-          backgroundColor: Colors.blue[200],
-        ),
-        body: ListView(
-          padding: EdgeInsets.all(15),
-          children: [
-            Text('How are you today?'),
-            const SizedBox(height: 20),
-            TextField(
-              maxLines: 20,
-              decoration: InputDecoration(
-                hintText: 'Start typing here...',
-                labelText: 'Start typing here...',
-                alignLabelWithHint: true,
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      appBar: AppBar(
+        title: const Text('My Diary'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 154, 187, 222),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/droppy.png'),
+                fit: BoxFit.cover,
               ),
-            )
-          ],
-        ));
+            ),
+          ),
+          Container(
+            color: Color.fromARGB(
+                255, 154, 187, 222), // Adjust the opacity and color as needed
+          ),
+          ListView(
+            padding: EdgeInsets.all(15),
+            children: [
+              const SizedBox(height: 100),
+              Text(
+                'How are you today?',
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                maxLines: 20,
+                decoration: InputDecoration(
+                  labelText: '05/01/2024 ',
+                  hintText: 'Start typing here...',
+                  filled: true,
+                  fillColor: Colors.white,
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
