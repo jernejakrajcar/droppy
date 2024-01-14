d71207635e11acb60ad23e59400729e2cf4845a3
smoki599 <69309225+smoki599@users.noreply.github.com>
2024-01-14 13:39:59 +0100

    Add files via upload

--- lib/screens/yoga/stretching_screen.dart
+++ lib/screens/yoga/stretching_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:droppy/screens/clock.dart';
 
 class StretchingScreen extends StatelessWidget {
-  const StretchingScreen({super.key});
+  const StretchingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -31,20 +32,22 @@ class StretchingScreen extends StatelessWidget {
 }
 
 class StretchingImageContainer extends StatelessWidget {
-  const StretchingImageContainer({super.key});
+  const StretchingImageContainer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Transform(
-          transform: Matrix4.identity()..translate(130.0, 50.0)..rotateZ(0.0),
+          transform: Matrix4.identity()
+            ..translate(130.0, 50.0)
+            ..rotateZ(0.0),
           child: Container(
-            width: 140, // Adjusted width to match StretchingImageContainer
-            height: 150, // Adjusted height to match StretchingImageContainer
+            width: 140,
+            height: 150,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage("assets/droppy.png"), // Updated to use local image
+                image: AssetImage("assets/droppy.png"),
                 fit: BoxFit.contain,
               ),
             ),
@@ -56,7 +59,7 @@ class StretchingImageContainer extends StatelessWidget {
 }
 
 class StretchingBottomContainer extends StatelessWidget {
-  const StretchingBottomContainer({super.key});
+  const StretchingBottomContainer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -69,13 +72,61 @@ class StretchingBottomContainer extends StatelessWidget {
         border: Border.all(color: const Color(0xFF5982C5), width: 2),
         borderRadius: BorderRadius.circular(15),
       ),
-      child: const Column(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          StretchingItemWidget('Downward dog', '45 sec'),
-          StretchingItemWidget('Warrior', '1 min'),
-          StretchingItemWidget('Tree pose', '35 sec'),
-          StretchingItemWidget('Corpse pose', '2 min'),
+          StretchingItemWidget(
+            title: 'Downward dog',
+            duration: '45 sec',
+            onTap: () {
+              // Navigate to the CountdownScreen with an initial duration of 45 seconds
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CountdownScreen(initialSeconds: 45),
+                ),
+              );
+            },
+          ),
+          StretchingItemWidget(
+            title: 'Warrior',
+            duration: '1 min',
+            onTap: () {
+              // Navigate to the CountdownScreen with an initial duration of 45 seconds
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CountdownScreen(initialSeconds: 60),
+                ),
+              );
+            },
+          ),
+          StretchingItemWidget(
+            title: 'Tree pose',
+            duration: '35 sec',
+            onTap: () {
+              // Navigate to the CountdownScreen with an initial duration of 45 seconds
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CountdownScreen(initialSeconds: 35),
+                ),
+              );
+            },
+          ),
+          StretchingItemWidget(
+            title: 'Corpse pose',
+            duration: '2 min',
+            onTap: () {
+              // Navigate to the CountdownScreen with an initial duration of 45 seconds
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CountdownScreen(initialSeconds: 120),
+                ),
+              );
+            },
+          ),
         ],
       ),
     );
@@ -85,32 +136,41 @@ class StretchingBottomContainer extends StatelessWidget {
 class StretchingItemWidget extends StatelessWidget {
   final String title;
   final String duration;
+  final VoidCallback? onTap;
 
-  const StretchingItemWidget(this.title, this.duration, {super.key});
+  const StretchingItemWidget({
+    required this.title,
+    required this.duration,
+    this.onTap,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 75,
-      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      decoration: BoxDecoration(
-        color: const Color(0xFF5982C5),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(color: Colors.white, fontSize: 18),
-          ),
-          Text(
-            duration,
-            style: const TextStyle(color: Colors.white, fontSize: 18),
-          ),
-        ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 75,
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          color: const Color(0xFF5982C5),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            Text(
+              duration,
+              style: const TextStyle(color: Colors.white, fontSize: 18),
+            ),
+          ],
+        ),
       ),
     );
   }
